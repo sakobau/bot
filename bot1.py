f3b69725b8536398e0b84a9374497280949843cc
sakobau <166872704+sakobau@users.noreply.github.com>
2024-09-01 13:21:02 +0300

    Update bot1.py

--- bot1.py
+++ bot1.py
@@ -25,16 +25,18 @@ def send_welcome(message):
 
 @bot.message_handler(func=lambda message: message.text == 'كارتات اسيا')
 def asia_cards_handler(message):
-    # إنشاء لوحة مفاتيح جديدة تحتوي على زر "5$" وزر "10$" وزر "رجوع"
+    # إنشاء لوحة مفاتيح جديدة تحتوي على زر "5$" وزر "10$" وزر "20$" وزر "رجوع"
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     
     btn_5dollars = types.KeyboardButton('5$')
-    btn_10dollars = types.KeyboardButton('10$')  # زر جديد
+    btn_10dollars = types.KeyboardButton('10$')  # زر 10$
+    btn_20dollars = types.KeyboardButton('20$')  # زر جديد 20$
     btn_back = types.KeyboardButton('رجوع')
     
     # إضافة الأزرار إلى اللوحة
     markup.add(btn_5dollars)
-    markup.add(btn_10dollars)  # إضافة الزر الجديد
+    markup.add(btn_10dollars)  # إضافة الزر 10$
+    markup.add(btn_20dollars)  # إضافة الزر الجديد 20$
     markup.add(btn_back)
     
     # إرسال رسالة مع اللوحة الجديدة
@@ -66,6 +68,19 @@ def ten_dollars_handler(message):
     # إرسال رسالة مع اللوحة الجديدة
     bot.send_message(message.chat.id, "ستتم عملية الاستقطاع من الرصيد بمقدار 12000. هل أنت موافق؟", reply_markup=markup)
 
+@bot.message_handler(func=lambda message: message.text == '20$')
+def twenty_dollars_handler(message):
+    # إنشاء لوحة مفاتيح مدمجة تحتوي على أزرار "نعم" و "لا"
+    markup = types.InlineKeyboardMarkup()
+    btn_yes = types.InlineKeyboardButton('نعم', callback_data='confirm_yes_20')
+    btn_no = types.InlineKeyboardButton('لا', callback_data='confirm_no')
+    
+    # إضافة الأزرار إلى اللوحة
+    markup.add(btn_yes, btn_no)
+    
+    # إرسال رسالة مع اللوحة الجديدة
+    bot.send_message(message.chat.id, "ستتم عملية الاستقطاع من الرصيد بمقدار 24000. هل أنت موافق؟", reply_markup=markup)
+
 @bot.callback_query_handler(func=lambda call: call.data == 'confirm_yes_5')
 def confirm_yes_5(call):
     bot.answer_callback_query(call.id, text="سيتم إرسال معلوماتك إلى المجهز وسيتم تجهيزك بالرصيد بأقرب وقت ممكن.")
@@ -76,6 +91,14 @@ def confirm_yes_5(call):
 
 @bot.callback_query_handler(func=lambda call: call.data == 'confirm_yes_10')
 def confirm_yes_10(call):
+    bot.answer_callback_query(call.id, text="سيتم إرسال معلوماتك إلى المجهز وسيتم تجهيزك بالرصيد بأقرب وقت ممكن.")
+    # إرسال رسالة تأكيدية
+    bot.send_message(call.message.chat.id, "شكراً لك! سيتم معالجة طلبك بأسرع وقت.")
+    # العودة إلى القائمة الرئيسية بعد معالجة الطلب
+    send_welcome(call.message)
+
+@bot.callback_query_handler(func=lambda call: call.data == 'confirm_yes_20')
+def confirm_yes_20(call):
     bot.answer_callback_query(call.id, text="سيتم إرسال معلوماتك إلى المجهز وسيتم تجهيزك بالرصيد بأقرب وقت ممكن.")
     # إرسال رسالة تأكيدية
     bot.send_message(call.message.chat.id, "شكراً لك! سيتم معالجة طلبك بأسرع وقت.")
